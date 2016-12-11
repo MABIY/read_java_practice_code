50a6f94a8f40ace249d75bd737796f13c8f19552
MABIY <jyfc7879@gmail.com>
2016-12-11 13:36:10 +0800

    第14章

--- Java编程思想(第4版)/src/fourteen_chapters/pets/PetCount3.java
+++ Java编程思想(第4版)/src/fourteen_chapters/pets/PetCount3.java
@@ -1,6 +1,9 @@
 package fourteen_chapters.pets;
 
+import net.mindview.util.MapData;
+
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by lh on 16-12-9.
@@ -8,7 +11,14 @@ import java.util.LinkedHashMap;
 public class PetCount3 {
     static class PetCounter extends LinkedHashMap<Class<? extends Pet>, Integer> {
         public PetCounter() {
-            super(MapData.map);
+            super(MapData.map(LiteralPetCreator.allTypes, 0));
+        }
+
+        public void count(Pet pet) {
+            // Class.isInstance() eliminates instanceofs
+            for (Map.Entry<Class<? extends Pet>, Integer> pair : entrySet()) {
+                if(pair)
+            }
         }
     }
 }
