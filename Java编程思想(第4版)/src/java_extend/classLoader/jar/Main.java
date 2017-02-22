dad34c60a1c3b6485ae3c4bf5fe2b622eb29cf33
MABIY <jyfc7879@gmail.com>
2017-02-22 15:53:50 +0800

    ..

--- Java编程思想(第4版)/src/java_extend/classLoader/jar/Main.java
+++ Java编程思想(第4版)/src/java_extend/classLoader/jar/Main.java
@@ -4,7 +4,9 @@ package java_extend.classLoader.jar;
  * Created by lh on 17-2-22.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("111");
+    private Main instance;
+
+    public void setSample(Object instance) {
+        this.instance = (Main) instance;
     }
 }
--- Java编程思想(第4版)/src/java_extend/classLoader/jar/URLClassLoaderTest.java
+++ Java编程思想(第4版)/src/java_extend/classLoader/jar/URLClassLoaderTest.java
@@ -12,12 +12,14 @@ import java.net.URLClassLoader;
  */
 public class URLClassLoaderTest {
     public static void main(String[] args) throws MalformedURLException, InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException {
-        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new URL("file://home/lh/test.jar")});
-        Main main = new Main();
-        Main.main(null);
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new URL("file:/home/lh/test.jar")});
+        URLClassLoader classLoader1 = URLClassLoader.newInstance(new URL[]{new URL("file:/home/lh/test.jar")});
+        // 2 个 ClassLoader 统一级别 不会互相类是否加载 , 系统 提供的Bootstrap ClassLoader Extension ClassLoader Application ClassLoader  所以是单例
         Class cls = classLoader.loadClass("java_extend.classLoader.jar.Main");
-        Method method = cls.getMethod("main", String[].class);
-        String[] params = new String[2];
-        method.invoke(null, (Object) params);
+        Object obj1 = cls.getInterfaces();
+        Class mm = classLoader1.loadClass("java_extend.classLoader.jar.Main");
+        Object obj2 = mm.getInterfaces();
+        Method method = cls.getMethod("setSample", java.lang.Object.class);
+        method.invoke(obj1,obj2);
     }
 }
