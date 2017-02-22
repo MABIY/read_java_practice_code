588da71ecbf65cd669da146f59e274350df38809
MABIY <jyfc7879@gmail.com>
2017-02-22 16:07:41 +0800

    ..

--- Java编程思想(第4版)/src/java_extend/classLoader/jar/URLClassLoaderTest.java
+++ Java编程思想(第4版)/src/java_extend/classLoader/jar/URLClassLoaderTest.java
@@ -11,14 +11,14 @@ import java.net.URLClassLoader;
  * Created by lh on 17-2-22.
  */
 public class URLClassLoaderTest {
-    public static void main(String[] args) throws MalformedURLException, InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException {
+    public static void main(String[] args) throws MalformedURLException, InvocationTargetException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException, InstantiationException {
         URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new URL("file:/home/lh/test.jar")});
         URLClassLoader classLoader1 = URLClassLoader.newInstance(new URL[]{new URL("file:/home/lh/test.jar")});
-        // 2 个 ClassLoader 统一级别 不会互相类是否加载 , 系统 提供的Bootstrap ClassLoader Extension ClassLoader Application ClassLoader  所以是单例
+        // 2 个 ClassLoader 同一级别 不会判断类是否加载 , 系统 提供的Bootstrap ClassLoader Extension ClassLoader Application ClassLoader  所以是单例
         Class cls = classLoader.loadClass("java_extend.classLoader.jar.Main");
-        Object obj1 = cls.getInterfaces();
+        Object obj1 = cls.newInstance();
         Class mm = classLoader1.loadClass("java_extend.classLoader.jar.Main");
-        Object obj2 = mm.getInterfaces();
+        Object obj2 = mm.newInstance();
         Method method = cls.getMethod("setSample", java.lang.Object.class);
         method.invoke(obj1,obj2);
     }
